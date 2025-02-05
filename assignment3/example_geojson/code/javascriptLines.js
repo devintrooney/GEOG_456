dc3c20602722d427480452038d96c902f29d8010
Devin Rooney <40042643+devintrooney@users.noreply.github.com>
2025-02-05 08:45:55 -0500

    commit

--- assignment3/example_geojson/code/javascriptLines.js
+++ assignment3/example_geojson/code/javascriptLines.js
@@ -1,68 +1,67 @@
 /// I am placing here a set of js lines that we will use in class
 /// However try not to depend entirely on copy-pasting so that way you get to learn better what is happening in the code
 
-var carolinaHall = [35.911276,-79.05004];
-
 // This is an object for assigning style to leaflet markers
-var geojsonMarkerOptions = {
+var markerOptions = {
     radius: 8,
-    fillColor: "#7BAFD4",
+    fillColor: "#000080",
     color: "#000",
     weight: 1,
     opacity: 1,
-    fillOpacity: 0.8
+    fillOpacity: 0.9
 };
 
-// var map = L.map('map').setView(carolinaHall, 17);
+/* var map = L.map('map').setView(carolinaHall, 17);
         
 var marker = L.marker(carolinaHall).bindPopup("<b>Carolina Hall</b>").addTo(map);
 
-L.circleMarker(carolinaHall,geojsonMarkerOptions).addTo(map);
+L.circleMarker(carolinaHall,markerOptions).addTo(map);
 
+*/
 
-// L.geoJSON(waypoints).addTo(map);
-var longitudeWpt0 = waypoints.features[0].geometry.coordinates[0]
-var latitudeWpt0 = waypoints.features[0].geometry.coordinates[1]
+// L.geoJSON(centroids).addTo(map);
+var districtLON = centroids.features[0].geometry.coordinates[0]
+var districtLAT = centroids.features[0].geometry.coordinates[1]
 
 // in this line I am getting the lat and lon of the first feature in the array
 // I use .slice().reverse() to reverse the original [lon,lat] to [lan,lon] since leaflet uses data in [lat,lon] format
-var firstWaypoint = waypoints.features[0].geometry.coordinates.slice().reverse()
+var firstCentroid = centroids.features[0].geometry.coordinates.slice().reverse()
 
 // in this line I am making a circle marker for the first waypoint in the array. 
-L.circleMarker(firstWaypoint,geojsonMarkerOptions).bindPopup(waypoints.features[0].properties.note ).addTo(map);
+L.circleMarker(firstCentroid,markerOptions).bindPopup(centroids.features[0].properties.note ).addTo(map);
 
 // This is using the method of pointToLayer from leaflet which iterates through every feature in the geojson 
-var myCircles = L.geoJSON(waypoints, {
+var cc = L.geoJSON(centroids, {
     pointToLayer: function (feature, latlng) {
         // console.log(feature)
-        return L.circleMarker(latlng, geojsonMarkerOptions).bindPopup(feature.properties.note );
+        return L.circleMarker(latlng, markerOptions).bindPopup(feature.properties.note);
     }
 }).addTo(map);
 
 /////////////////////////////////////
 //Working with array methods in js //
 /////////////////////////////////////
-
+/*
 // the map method creates a new array 
-myFeatures = waypoints.features.map(e => e)
+myFeatures = centroids.features.map(e => e)
 // in the previous line I just created a new array called myFeatures
 // in this line I am printing out the first longitude in the array of features.
 console.log(myFeatures[0].geometry.coordinates[0])
 
 // i am creating an array of longitudes and of latitudes
-myLongitudes = waypoints.features.map(e => e.geometry.coordinates[0])
-myLatitudes = waypoints.features.map(e => e.geometry.coordinates[1])
+myLongitudes = centroids.features.map(e => e.geometry.coordinates[0])
+myLatitudes = centroids.features.map(e => e.geometry.coordinates[1])
 
 
 // the forEach() method is similar to map(), but does not create a new array
-waypoints.features.forEach(e => console.log('long = ' + e.geometry.coordinates[0]))
+centroids.features.forEach(e => console.log('long = ' + e.geometry.coordinates[0]))
 
-myCircles.remove() // removing the leaflet layer
-geojsonMarkerOptions.fillColor = 'red' // changing the fill color property
-waypoints.features.forEach(e => L.circleMarker(e.geometry.coordinates.slice().reverse(),geojsonMarkerOptions).addTo(map));
+//cc.remove() // removing the leaflet layer
+//markerOptions.fillColor = 'red' // changing the fill color property
+// centroids.features.forEach(e => L.circleMarker(e.geometry.coordinates.slice().reverse(),markerOptions).addTo(map));
 // the previous line demonstrates how I can use the forEach() iteration to generate the same result as the L.geoJSON method
 // this works as long as I give the coordinates in lat/lon.  
-
+*/
 //////////////////////////////////////
 //Working with leaflet layer controls/
 //////////////////////////////////////
@@ -83,7 +82,8 @@ var baseLayers = {
 // this is an object with the vectors
 // the property or key is the text and the value is the variable holding the vectors
 var overlayMaps = {
-    "Circles": myCircles
+    "Districts": districtBoundaries
+    "Circles": cc
 };
 // This method will create the button for controlling layers
 var layerControl = L.control.layers(baseLayers,overlayMaps).addTo(map);
@@ -92,7 +92,7 @@ var layerControl = L.control.layers(baseLayers,overlayMaps).addTo(map);
 //Working with leaflet layer events///
 //////////////////////////////////////
 
-// here I am creating an empty variable
+/* here I am creating an empty variable
 var daEvent;
 // here I am creating a empty leaflet popup object using the .popup() method
 var popup = L.popup();
--- example_geojson/code/yourCode.js
+++
@@ -1,5 +0,0 @@
-var marker = L.marker([35.911276,-79.05004]).bindPopup("<b>Carolina Hall</b>").addTo(map);
-var OSM = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
